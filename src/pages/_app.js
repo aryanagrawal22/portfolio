92f908605b9b20f64fd57c60e05187af58b1d583
aryanagrawal22 <aryanagrawal20023@gmail.com>
2022-03-05 18:25:34 +0530

    App: Meta Data Added

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -9,6 +9,12 @@ function MyApp({ Component, pageProps }) {
       <Head>
         <link rel="shortcut icon" href="favicon.ico" />
         <title>Aryan Agrawal | Portfolio</title>
+        <meta property="og:url" content="https://aryanagrawal.in"/>
+        <meta name="title" property="og:title" content="Aryan Agrawal | Portfolio" />
+        <meta property="og:type" content="Website" />
+        <meta name="image" property="og:image" content="images/thumbnail.PNG" />
+        <meta name="description" property="og:description" content="I am Innovative, task-driven, and enthusiastic with knowledge of Web-Development and creating highly scalable and distributed systems" />
+        <meta name="author" content="Aryan Agrawal" />
       </Head>
       <Component {...pageProps} />
     </>
