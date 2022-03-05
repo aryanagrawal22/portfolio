5a0bc13452e57c0a599dfa9467d52379ebb89dc5
aryanagrawal22 <aryanagrawal20023@gmail.com>
2022-03-05 18:35:38 +0530

    App: Link Changes

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -12,7 +12,7 @@ function MyApp({ Component, pageProps }) {
         <meta property="og:url" content="https://aryanagrawal.in"/>
         <meta name="title" property="og:title" content="Aryan Agrawal | Portfolio" />
         <meta property="og:type" content="Website" />
-        <meta name="image" property="og:image" content="images/thumbnail.PNG" />
+        <meta name="image" property="og:image" content="https://aryanagrawal.in/images/thumbnail.PNG" />
         <meta name="description" property="og:description" content="I am Innovative, task-driven, and enthusiastic with knowledge of Web-Development and creating highly scalable and distributed systems" />
         <meta name="author" content="Aryan Agrawal" />
       </Head>
